2bb41816420c16939eaba451a852b2bd72cc6b31
Alexis Delgado <alexisndelgado@gmail.com>
2023-09-19 15:41:21 -0300

    fix: input name

--- push_repository.sh
+++ push_repository.sh
@@ -1,6 +1,6 @@
 AUTH='Authorization: Bearer '$API_KEY
-DATA=`PWD`/module.zip
-zip -r module.zip $PATH -x .git\* -x push_repository.sh
+DATA=./modules.zip
+zip -r modules.zip $MODULE_PATH -x .git\* -x push_repository.sh
 echo ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
 wget --no-check-certificate -v --method POST --timeout=0 --header "$AUTH" --header 'Content-Type: application/zip' \
         --body-file="$DATA" ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
