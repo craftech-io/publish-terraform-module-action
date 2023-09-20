903a730232b686b00171f1e7efe9d3f651b2f702
Alexis Delgado <alexisndelgado@gmail.com>
2023-09-20 12:22:22 -0300

    fix: error messages

--- push_repository.sh
+++ push_repository.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e 
+
 AUTH='Authorization: Bearer '$API_KEY
 DATA=./modules.zip
 
 function log_error {
         local -ra message=("$@")
-        log "ERROR" "${message[@]}"
+        echo "ERROR: ${message[@]}"
 }
 
 function log_info {
         local -ra message=("$@")
-        log "INFO" "${message[@]}"
+        echo "INFO: ${message[@]}"
 }
 
-function chef_if_empty {
+function check_if_empty {
         if [ -z "$1" ]; then
                 log_error "The '$2' cannot be empty"
         fi
@@ -27,10 +29,10 @@ else
         log_error "The hostname '$HOSTNAME' is invalid."
 fi
 
-chef_if_empty $API_KEY "API Key"
-chef_if_empty $NAMESPACE "namespace"
-chef_if_empty $NAME "name"
-chef_if_empty $VERSION "version"
+check_if_empty $API_KEY "API Key"
+check_if_empty $NAMESPACE "namespace"
+check_if_empty $NAME "name"
+check_if_empty $VERSION "version"
 
 if [ ! -d "$MODULES_PATH" ]; then
     log_error "The directory '$MODULES_PATH' does not exists."
