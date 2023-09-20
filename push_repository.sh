eb0eddf50898e133d6f9a2349df7be96499ae581
Alexis Delgado <alexisndelgado@gmail.com>
2023-09-20 14:40:53 -0300

    chore: validates that env vars are not empty

--- push_repository.sh
+++ push_repository.sh
@@ -6,34 +6,36 @@ AUTH='Authorization: Bearer '$API_KEY
 DATA=./modules.zip
 
 function log_error {
-        local -ra message=("$@")
-        echo "ERROR: ${message[@]}"
+        echo "ERROR: $1"
+        exit 1
 }
-
-function log_info {
-        local -ra message=("$@")
-        echo "INFO: ${message[@]}"
-}
-
-function check_if_empty {
-        if [ -z "$1" ]; then
-                log_error "The '$2' cannot be empty"
-        fi
-}
-
-check_if_empty $HOSTNAME "hostname"
 
 if [[ ! "$HOSTNAME" =~ ^https://.*[^/]$ ]]; then
         log_error "The hostname '$HOSTNAME' is invalid."
 fi
 
-check_if_empty $API_KEY "API Key"
-check_if_empty $NAMESPACE "namespace"
-check_if_empty $NAME "name"
-check_if_empty $VERSION "version"
+if [ -z "$HOSTNAME" ]; then
+        log_error "The hostname cannot be empty"
+fi
 
-if [ ! -d "$MODULES_PATH" ]; then
-    log_error "The directory '$MODULES_PATH' does not exists."
+if [ -z "$API_KEY" ]; then
+        log_error "The API Key cannot be empty"
+fi
+
+if [ -z "$NAMESPACE" ]; then
+        log_error "The namespace cannot be empty"
+fi
+
+if [ -z "$NAME" ]; then
+        log_error "The name cannot be empty"
+fi
+
+if [ -z "$VERSION" ]; then
+        log_error "The version cannot be empty"
+fi
+
+if [ ! -d "$MODULE_PATH" ]; then
+    log_error "The directory '$MODULE_PATH' does not exists."
 fi
 
 zip -r modules.zip $MODULE_PATH -x .git\* -x push_repository.sh
