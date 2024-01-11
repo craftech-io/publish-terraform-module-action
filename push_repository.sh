4130faca8e973dad5397b7a2bcde6d75d5e059b4
Patricio Costilla <patriciocostilla@gmail.com>
2024-01-11 10:38:57 -0300

    fix: clean tf module version before uploading

--- push_repository.sh
+++ push_repository.sh
@@ -43,7 +43,9 @@ if [ -n "$LOWER_TERRAFORM_VERSION" ] || [ -n "$HIGHER_TERRAFORM_VERSION" ]; then
         echo 'The terraform required versions are compliant with the desired version range.'
 fi
 
+CLEAN_VERSION=${VERSION#v}
+
 zip -r modules.zip $MODULES_PATH -x .git\* -x push_repository.sh -x terraform_required_versions.py
-echo ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
+echo ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$CLEAN_VERSION"'/upload'
 wget --no-check-certificate -v --method POST --timeout=0 --header "$AUTH" --header 'Content-Type: application/zip' \
-        --body-file="$DATA" ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
+        --body-file="$DATA" ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$CLEAN_VERSION"'/upload'
