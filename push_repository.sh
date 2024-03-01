e79b1c88ce479fddadf7b7054e9699e904330e1c
Patricio Costilla <patriciocostilla@gmail.com>
2024-03-01 17:26:15 -0300

    fix: uncomment lines

--- push_repository.sh
+++ push_repository.sh
@@ -2,51 +2,51 @@
 
 set -e 
 
-# AUTH='Authorization: Bearer '$API_KEY
-# DATA=./modules.zip
+AUTH='Authorization: Bearer '$API_KEY
+DATA=./modules.zip
 
-# function log_error {
-#         echo "ERROR: $1"
-#         exit 1
-# }
+function log_error {
+        echo "ERROR: $1"
+        exit 1
+}
 
-# if [[ ! "$HOSTNAME" =~ ^https://.*[^/]$ ]]; then
-#         log_error "The hostname '$HOSTNAME' is invalid."
-# fi
+if [[ ! "$HOSTNAME" =~ ^https://.*[^/]$ ]]; then
+        log_error "The hostname '$HOSTNAME' is invalid."
+fi
 
-# if [ -z "$HOSTNAME" ]; then
-#         log_error "The hostname cannot be empty"
-# fi
+if [ -z "$HOSTNAME" ]; then
+        log_error "The hostname cannot be empty"
+fi
 
-# if [ -z "$API_KEY" ]; then
-#         log_error "The API Key cannot be empty"
-# fi
+if [ -z "$API_KEY" ]; then
+        log_error "The API Key cannot be empty"
+fi
 
-# if [ -z "$NAMESPACE" ]; then
-#         log_error "The namespace cannot be empty"
-# fi
+if [ -z "$NAMESPACE" ]; then
+        log_error "The namespace cannot be empty"
+fi
 
-# if [ -z "$NAME" ]; then
-#         log_error "The name cannot be empty"
-# fi
+if [ -z "$NAME" ]; then
+        log_error "The name cannot be empty"
+fi
 
-# VERSION=${VERSION#v}
+VERSION=${VERSION#v}
 
-# if [ -z "$VERSION" ]; then
-#         log_error "The version cannot be empty"
-# fi
+if [ -z "$VERSION" ]; then
+        log_error "The version cannot be empty"
+fi
 
-# if [ ! -d "$MODULES_PATH" ]; then
-#         log_error "The directory '$MODULES_PATH' does not exists."
-# fi
+if [ ! -d "$MODULES_PATH" ]; then
+        log_error "The directory '$MODULES_PATH' does not exists."
+fi
 
-# if [ -n "$LOWER_TERRAFORM_VERSION" ] || [ -n "$HIGHER_TERRAFORM_VERSION" ]; then
-#         python3 /terraform_required_versions.py
-#         echo 'The terraform required versions are compliant with the desired version range.'
-# fi
+if [ -n "$LOWER_TERRAFORM_VERSION" ] || [ -n "$HIGHER_TERRAFORM_VERSION" ]; then
+        python3 /terraform_required_versions.py
+        echo 'The terraform required versions are compliant with the desired version range.'
+fi
 
-# zip -r modules.zip $MODULES_PATH -x .git\* push_repository.sh terraform_required_versions.py requirements.txt $EXCLUDE
-# echo ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
+zip -r modules.zip $MODULES_PATH -x .git\* push_repository.sh terraform_required_versions.py requirements.txt $EXCLUDE
+echo ''"$HOSTNAME"'/'"$NAMESPACE"'/'"$NAME"'/'"$SYSTEM"'/'"$VERSION"'/upload'
 
 if [ "$DRY_RUN" = "false" ]; then
         wget --no-check-certificate -v --method POST --timeout=0 --header "$AUTH" --header 'Content-Type: application/zip' \
